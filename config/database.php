d4177403662b43884fa5b3584906bf34b944e17d
Gabrielly Pontes <developergabrielly@gmail.com>
2025-12-08 21:39:02 -0300

    Remover pasta config do repositório

--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "wagnerweinert.com.br";
-$db   = "tads24_gabrielly";
-$user = "tads24_gabrielly";
-$pass = "tads24_gabrielly";
-
-$conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
-?>
